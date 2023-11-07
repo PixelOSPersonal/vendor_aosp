zhantech/android_vendor_rice

vendor: Import font modifications from protonAOSP
Signed-off-by: str-br <alexfinhart@gmail.com>

config/common.mk
@@ -3,6 +3,7 @@ $(call inherit-product-if-exists, vendor/extra/product.mk)
 $(call inherit-product-if-exists, vendor/addons/config.mk)
 $(call inherit-product-if-exists, vendor/lineage/config/rice.mk)
 $(call inherit-product-if-exists, vendor/lineage/audio/audio.mk)
+$(call inherit-product-if-exists, vendor/lineage/fonts/fonts.mk)
 
 PRODUCT_BRAND ?= riceDroid
 

fonts/font_files.mk
@@ -1,0 +1,40 @@
+# Copyright (C) 2021 The Proton AOSP Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := vendor/proton/fonts
+
+# We have to use PRODUCT_PACKAGES (together with BUILD_PREBUILT) instead of
+# PRODUCT_COPY_FILES to install the font files, so that the NOTICE file can
+# get installed too.
+
+PRODUCT_PACKAGES += \
+    CutiveMono.ttf \
+    DroidSansMono.ttf \
+    Roboto-Regular.ttf \
+    Manrope-VF.ttf \
+    NotoSans-Italic-VF.ttf \
+    NotoSans-VF.ttf \
+    Recursive-VF.ttf \
+    RobotoFallback-VF.ttf \
+    RobotoSystem-VF.ttf \
+    RobotoStatic-Regular.ttf \
+    SourceSans3-Italic-VF.otf \
+    SourceSans3-VF.otf \
+    NotoSerif-BoldItalic.ttf \
+    NotoSerif-Bold.ttf \
+    NotoSerif-Italic.ttf \
+    NotoSerif-Regular.ttf \
+
+# Include extra addon fonts
+-include vendor/lineage/fonts/extra/extra.mk

fonts/fonts.mk
@@ -1,0 +1,30 @@
+# Copyright (C) 2021 The Proton AOSP Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := vendor/lineage/fonts
+
+# Copy font files
+include vendor/lineage/fonts/font_files.mk
+-include vendor/lineage/fonts/extra/extra.mk
+
+# Overlays for UI font styles
+PRODUCT_PACKAGES += \
+    FontHKGroteskOverlay \
+    FontManropeOverlay \
+    FontNotoSansOverlay \
+    FontRecursiveCasualOverlay \
+    FontRecursiveLinearOverlay \
+    FontRobotoOverlay \
+    FontSourceSansOverlay \
+    FontSourceSerifOverlay \

PixelOSPersonal/vendor_aosp

Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

config/common.mk
@@ -1,5 +1,6 @@
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)
+$(call inherit-product-if-exists, vendor/aosp/fonts/fonts.mk)
 
 # Audio
 $(call inherit-product, vendor/aosp/audio/audio.mk)

fonts/font_files.mk
@@ -1,0 +1,40 @@
+# Copyright (C) 2021 The Proton AOSP Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := vendor/proton/fonts
+
+# We have to use PRODUCT_PACKAGES (together with BUILD_PREBUILT) instead of
+# PRODUCT_COPY_FILES to install the font files, so that the NOTICE file can
+# get installed too.
+
+PRODUCT_PACKAGES += \
+    CutiveMono.ttf \
+    DroidSansMono.ttf \
+    Roboto-Regular.ttf \
+    Manrope-VF.ttf \
+    NotoSans-Italic-VF.ttf \
+    NotoSans-VF.ttf \
+    Recursive-VF.ttf \
+    RobotoFallback-VF.ttf \
+    RobotoSystem-VF.ttf \
+    RobotoStatic-Regular.ttf \
+    SourceSans3-Italic-VF.otf \
+    SourceSans3-VF.otf \
+    NotoSerif-BoldItalic.ttf \
+    NotoSerif-Bold.ttf \
+    NotoSerif-Italic.ttf \
+    NotoSerif-Regular.ttf \
+
+# Include extra addon fonts
+-include vendor/aosp/fonts/extra/extra.mk

fonts/fonts.mk
@@ -1,0 +1,30 @@
+# Copyright (C) 2021 The Proton AOSP Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := vendor/aosp/fonts
+
+# Copy font files
+include vendor/aosp/fonts/font_files.mk
+-include vendor/aosp/fonts/extra/extra.mk
+
+# Overlays for UI font styles
+PRODUCT_PACKAGES += \
+    FontHKGroteskOverlay \
+    FontManropeOverlay \
+    FontNotoSansOverlay \
+    FontRecursiveCasualOverlay \
+    FontRecursiveLinearOverlay \
+    FontRobotoOverlay \
+    FontSourceSansOverlay \
+    FontSourceSerifOverlay \